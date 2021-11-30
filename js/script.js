0f0a9f2f1c551ae7d76edf1d518e88bc47f1c378
Sam Garcia <samuel.gsuero@gmail.com>
2021-11-30 14:19:35 +0100

    pokemon list added
    the list displays names / height / types of each pokemon added

---
+++ js/script.js
@@ -0,0 +1,5 @@
+pokemonList = [
+  {name: 'Ponyta', height: 1, types: ['fire']},
+  {name: 'Lugia', height: 5.2, types: ['psychic', 'flying']},
+  {name: 'Rayuaza', height: 7, types: ['dragon', 'flying']}
+];
