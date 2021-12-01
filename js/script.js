4b4e0ba4d7bb560de0cce273122109ad328a441e
Sam Garcia <samuel.gsuero@gmail.com>
2021-12-01 11:06:12 +0100

    for loop that displays names / height of each pokemon in the list

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-pokemonList = [
+let pokemonList = [
   {name: 'Ponyta', height: 1, types: ['fire']},
   {name: 'Lugia', height: 5.2, types: ['psychic', 'flying']},
-  {name: 'Rayuaza', height: 7, types: ['dragon', 'flying']}
+  {name: 'Rayquaza', height: 7, types: ['dragon', 'flying']}
 ];
+
+for (let i=0; i<pokemonList.length; i++){
+  document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height +  ')</p>');
+}
