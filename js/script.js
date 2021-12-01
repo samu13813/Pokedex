2de5e8a502e23c3399a3208b85179f5feb266fa9
Sam Garcia <samuel.gsuero@gmail.com>
2021-12-01 11:16:53 +0100

    for loop updated / comments added
    for loop now checks height of each pokemon in the list and if its larger than 6, displays a text.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,13 @@ let pokemonList = [
   {name: 'Rayquaza', height: 7, types: ['dragon', 'flying']}
 ];
 
+// for loop that displays names / heights of each pokemon in the above list
+//and checks if the height is larger than 6, and if it is, displays a text
+
 for (let i=0; i<pokemonList.length; i++){
-  document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height +  ')</p>');
+  if (pokemonList[i].height >= 6){
+    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height +  ') - Wow, that\'s big!</p>');
+  } else {
+    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height +  ')</p>');
+  }
 }
