a50f1ed6726909a92bf3533cf8108ffa69db6b88
Sam Garcia <samuel.gsuero@gmail.com>
2021-12-09 14:33:22 +0100

    pokemonList now is an IIFE and for loop updated

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
-let pokemonList = [
-  {name: 'Ponyta', height: 1, types: ['fire']},
-  {name: 'Lugia', height: 5.2, types: ['psychic', 'flying']},
-  {name: 'Rayquaza', height: 7, types: ['dragon', 'flying']}
-];
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {name: 'Ponyta', height: 1, types: ['fire']},
+    {name: 'Lugia', height: 5.2, types: ['psychic', 'flying']},
+    {name: 'Rayquaza', height: 7, types: ['dragon', 'flying']}
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+
+})();
 
 // for loop that displays names / heights of each pokemon in the above list
-//and checks if the height is larger than 6, and if it is, displays a text
+// and checks if the height is larger than 6, and if it is, displays a text
 
-pokemonList.forEach(function(item){
+pokemonRepository.getAll().forEach(function(item){
   if (item.height >= 6){
     document.write('<p>' + item.name + ' (height: ' + item.height +  ') - Wow, that\'s big!</p>');
   } else {
